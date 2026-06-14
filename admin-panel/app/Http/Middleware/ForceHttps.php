d5a8bcdd3b73f470a97453aceaac8f50b052b8c8
jhulio435m <jhulio435@gmail.com>
2026-06-14 03:05:31 -0500

    fix: production stability fixes for HTTPS, Vite, and routing

---
+++ admin-panel/app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceHttps
+{
+    public function handle(Request \$request, Closure \$next): Response
+    {
+        URL::forceScheme('https');
+        return \$next(\$request);
+    }
+}
